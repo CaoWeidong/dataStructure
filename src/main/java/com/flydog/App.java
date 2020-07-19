0a6d4d0c2f7014aa1189b405066b4f5194d3bb38
caoweidong <caoweidong1982@126.com>
2020-07-19 09:23:36 +0800

    first commit

---
+++ src/main/java/com/flydog/App.java
@@ -0,0 +1,13 @@
+package com.flydog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
